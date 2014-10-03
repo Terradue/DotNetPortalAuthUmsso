36c930d66f8fdf621caf4503d2a1b7aab44b65a1
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-10-03 10:42:37 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Authentication.Umsso/trunk@38329 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

---
+++ Resources/db/db-create.sql
@@ -0,0 +1,22 @@
+-- VERSION 1.4
+
+USE $MAIN$;
+
+/*****************************************************************************/
+
+-- Adding configuration variables for UM-SSO ... \
+INSERT INTO configsection (name, pos) VALUES
+    ('UM-SSO', 8)
+;
+
+SET @section = (SELECT LAST_INSERT_ID());
+
+INSERT INTO config (id_section, pos, name, type, source, caption, hint, value, optional) VALUES
+    (@section, 1, 'UmssoIdpBindingUrl', 'string', NULL, 'UM-SSO IDP Binding Web Service Endpoint', 'Enter the SOAP endpoint URL of the binding web service', NULL, true),
+    (@section, 2, 'UmssoSpCertFile', 'string', NULL, 'Location of the UM-SSO SP Certificate File (P12)', 'Enter the location of the PKCS12-formatted UM-SSO SP certificate file to be used for authentication with the UM-SSO IDP', NULL, true)
+;
+
+INSERT INTO auth (`identifier`, `name`, `description`, `type`, `enabled`) VALUES ('umsso', 'UM-SSO authentication', 'UM-SSO authentication allows users to identify themselves using ESA um-sso provider.', 'Terradue.Umsso.UmssoAuthenticationType, Terradue.Umsso', '1');
+-- RESULT
+
+/*****************************************************************************/
