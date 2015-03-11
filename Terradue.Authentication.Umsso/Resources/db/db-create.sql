fffcecfa5f629700a9bab48e732349a0fb53e3a2
frank.loeschau@terradue.com <floeschau@users.noreply.github.com>
2015-03-11 10:28:58 +0000

    [Terradue.Authentication.Umsso] Changed dependencies

--- Terradue.Authentication.Umsso/Resources/db/db-create.sql
+++ Terradue.Authentication.Umsso/Resources/db/db-create.sql
@@ -16,7 +16,10 @@ INSERT INTO config (id_section, pos, name, type, source, caption, hint, value, o
     (@section, 2, 'UmssoSpCertFile', 'string', NULL, 'Location of the UM-SSO SP Certificate File (P12)', 'Enter the location of the PKCS12-formatted UM-SSO SP certificate file to be used for authentication with the UM-SSO IDP', NULL, true)
 ;
 
-INSERT INTO auth (`identifier`, `name`, `description`, `type`, `enabled`) VALUES ('umsso', 'UM-SSO authentication', 'UM-SSO authentication allows users to identify themselves using ESA um-sso provider.', 'Terradue.Authentication.Umsso.UmssoAuthenticationType, Terradue.Authentication.Umsso', '1');
+SET @pos = (SELECT MAX(pos) FROM auth);
+INSERT INTO auth (pos, identifier, name, description, type, enabled) VALUES 
+    (@pos + 1, 'umsso', 'UM-SSO authentication', 'UM-SSO authentication allows users to identify themselves using ESA um-sso provider.', 'Terradue.Authentication.Umsso.UmssoAuthenticationType, Terradue.Authentication.Umsso', '1')
+;
 -- RESULT
 
 /*****************************************************************************/
